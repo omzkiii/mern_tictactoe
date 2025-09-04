1c3bcc022d3bfd3160327c4070bb9948a387aa68
omzkiii <geogeomarsantos@gmail.com>
2025-09-04 15:19:29 +0800

    add docker

--- backend/main.ts
+++ backend/main.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
